009bc853c43ae4b2a7d3e8d18055854131565d3d
Ryan Moakes <m015485n@student.staffs.ac.uk>
2024-01-24 20:19:35 +0000

    Added Lerping to the inventory UI

--- CSharpForEngines1-main/Assets/Scripts/InventoryUPDOWN.cs
+++ CSharpForEngines1-main/Assets/Scripts/InventoryUPDOWN.cs
@@ -6,32 +6,61 @@ using UnityEngine.UIElements;
 public class InventoryUPDOWN : MonoBehaviour
 {
 
-    public GameObject inventory;
+    
+    private float transitionSpeed = 5f;
+    private bool isMoving = false;
+    RectTransform rectTransform;
 
     private void Awake()
     {
-        if (inventory == null)
-        {
-            Debug.Log("No Inventory Assigned");
-        }
+        //Yoink
+        rectTransform = GetComponent<RectTransform>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        // You could probably get away with just clamping the position when the player presses E instead of every frame, but im not smart enough to figure that out
+        ClampPosition();
+
+        if (Input.GetKeyDown(KeyCode.E) && isMoving == false)
         {
-            Vector3 inventoryPos = inventory.transform.position;
-            inventory.transform.position = new Vector3(inventoryPos.x, inventoryPos.y + 200, inventoryPos.z);
-           
-        }
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            Vector3 inventoryPos = inventory.transform.position;
-            inventory.transform.position = new Vector3(inventoryPos.x, inventoryPos.y - 200, inventoryPos.z);
+            StartCoroutine(MoveInventory(200));
 
         }
-
+        if (Input.GetKeyUp(KeyCode.E) && isMoving == false)
+        {
+            StartCoroutine(MoveInventory(-200));
+        }
+        
     }
+    // Real men use functions
+    void ClampPosition()
+    {
+        Vector3 currentPosition = rectTransform.anchoredPosition;
+        currentPosition.y = Mathf.Clamp(currentPosition.y, -200, 0);
 
+        rectTransform.anchoredPosition = currentPosition;
+    }
+    // I hate coroutines
+    IEnumerator MoveInventory(float yOffset)
+    {
+        isMoving = true;
+        Vector3 startPos = transform.position;
+        Vector3 targetPos = new Vector3(startPos.x, startPos.y + yOffset, startPos.z);
+        float elapsedTime = 0f;
+       
+
+        while (elapsedTime < 1f)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime);
+            elapsedTime += Time.deltaTime * transitionSpeed;
+             yield return null;
+        }
+       
+       
+        transform.position = targetPos;
+        isMoving = false;
+       
+    }
 
 }
